dbcde49842f9868203c366f0ee2148046d371557
NathanBernard <nbx-off@outlook.fr>
2026-03-25 11:01:50 +0100

    feat: Made new gif btn work

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const img = document.querySelector("img");
+const randomGifBtn = document.getElementById("randomGifBtn");
 
 fetch(
   "https://api.giphy.com/v1/gifs/translate?api_key=n5AUEykERIPKKOwuk3CDQUaRHE0vzvfY&s=vaporwave",
@@ -9,3 +10,15 @@ fetch(
   .then(function (response) {
     img.src = response.data.images.original.url;
   });
+
+randomGifBtn.addEventListener("click", () => {
+  fetch(
+    "https://api.giphy.com/v1/gifs/translate?api_key=n5AUEykERIPKKOwuk3CDQUaRHE0vzvfY&s=vaporwave",
+  )
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (response) {
+      img.src = response.data.images.original.url;
+    });
+});
