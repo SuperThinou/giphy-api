3931662e5a7b60ceb4de072c67b108a9481380a7
NathanBernard <nbx-off@outlook.fr>
2026-03-25 10:59:02 +0100

    feat: Random vaporwave GIF is displayed on page load

--- script.js
+++ script.js
@@ -2,6 +2,10 @@ const img = document.querySelector("img");
 
 fetch(
   "https://api.giphy.com/v1/gifs/translate?api_key=n5AUEykERIPKKOwuk3CDQUaRHE0vzvfY&s=vaporwave",
-).then(function (response) {
-  console.log(response.json());
-});
+)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (response) {
+    img.src = response.data.images.original.url;
+  });
